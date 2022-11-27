38d34b4417e036361775fa589db19bbce4272dac
Gaurav Subhash Yawale <gauravs@geekyants.com>
2022-11-27 14:21:23 +0530

    🎉initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "antd";
+
+export default function Home() {
+  return (
+    <Typography.Text>
+      Customized login with Google and Credentials provider using next auth in
+      next.js
+    </Typography.Text>
+  );
+}
